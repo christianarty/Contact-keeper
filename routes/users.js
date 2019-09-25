77ac7fceaee5c523587ffbbe2935390539048384
Christian <artychristian@gmail.com>
2019-09-25 00:34:35 -0400

    Added the User model, added the parser middleware to server, and validated data using express-validator

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
 
 // @route    POST api/users
 // @desc     Register a user
 // @access   Public
-router.post('/', (request, response) => {
-	response.send('Register a user')
-})
+router.post(
+	'/',
+	[
+		check('name', 'Please add name')
+			.not()
+			.isEmpty(),
+		check('email', 'Please include a valid email').isEmail(),
+		check(
+			'password',
+			'Please enter a password with six or more characters'
+		).isLength({ min: 6 })
+	],
+	(request, response) => {
+		const errors = validationResult(request);
+		if (!errors.isEmpty()){return response.status(400).json({errors: errors.array()})}
+		response.send('Passed');
+	}
+);
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const connectDB = require('./config/db');
 
 app.get('/', (request, response) =>
 	response.json({ msg: 'Welcome to the Contact Keeper API...' })
-)
+);
 //Connect Database
 connectDB();
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 // Define our routes
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
