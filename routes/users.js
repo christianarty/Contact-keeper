517eeccc8f0b9493c9f82f0ed573d332f0defbb1
Christian <artychristian@gmail.com>
2019-09-25 19:38:26 -0400

    Added hashing to the User model using bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 
@@ -18,10 +19,33 @@ router.post(
 			'Please enter a password with six or more characters'
 		).isLength({ min: 6 })
 	],
-	(request, response) => {
+	async (request, response) => {
 		const errors = validationResult(request);
-		if (!errors.isEmpty()){return response.status(400).json({errors: errors.array()})}
-		response.send('Passed');
+		if (!errors.isEmpty()) {
+			return response.status(400).json({ errors: errors.array() });
+		}
+		const { name, email, password } = request.body;
+
+		try {
+			let user = await User.findOne({ email });
+			if (user) {
+				return response.status(400).json({ msg: 'User already exists' });
+			}
+			user = new User({
+				name,
+				email,
+				password
+			});
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+
+			await user.save();
+
+			response.send('User saved');
+		} catch (err) {
+			console.error(err.message);
+			response.status(500).send({msg: 'Sever Error saving User to DB'})
+		}
 	}
 );
 
