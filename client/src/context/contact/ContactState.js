81e5b60d865852ce89a70d9748255a19d28f55ac
Christian <artychristian@gmail.com>
2019-09-29 14:00:09 -0400

    Added the Contacts and ContactItem components and included ContextAPI for global state

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,68 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import ContactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  UPDATE_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from "../types";
+
+const ContactState = props => {
+  const initalState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Jill Johnson",
+        email: "jilljohn@gmail.com",
+        phone: "144-436-4722",
+        contactType: "personal"
+      },
+      {
+        id: 2,
+        name: "Harry White",
+        email: "harryw@gmail.com",
+        phone: "344-436-4821",
+        contactType: "personal"
+      },
+      {
+        id: 3,
+        name: "Jane Doe",
+        email: "janeD@gmail.com",
+        phone: "123-456-7891",
+        contactType: "professional"
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(ContactReducer, initalState);
+
+  //Add contact
+
+  //Delete contact
+
+  //Set current contact
+
+  //Clear current contact
+
+  //Update contact
+
+  //Filter contacts
+
+  //Clear filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+export default ContactState;
