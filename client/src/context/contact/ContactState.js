7b1c7ed6098bbec2d2c7aff2db869f5483ef4b5a
Christian <artychristian@gmail.com>
2019-11-06 10:06:27 -0500

    Added update feature with connection to backend

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -51,6 +51,25 @@ const ContactState = props => {
     }
   };
 
+  //Update contact
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    try {
+      const response = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({ type: UPDATE_CONTACT, payload: response.data });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+    }
+  };
   //Delete contact
   const deleteContact = async id => {
     try {
@@ -74,10 +93,6 @@ const ContactState = props => {
   //Clear current contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-  //Update contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
   //Filter contacts
   const filterContacts = text => {
