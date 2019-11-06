62153be6a7c5e64aa3a16e4c7e8a2b1eedb8f473
Christian <artychristian@gmail.com>
2019-11-06 10:02:00 -0500

    Added updateContact to respond to backend

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -2,11 +2,11 @@ import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import ContactContext from "../../context/contact/contactContext";
 const ContactItem = ({ contact }) => {
-  const { id, name, email, phone, contactType } = contact;
+  const { _id, name, email, phone, contactType } = contact;
   const contactContext = useContext(ContactContext);
   const {deleteContact, setCurrent, clearCurrent} = contactContext
   const onDelete = (e) => {
-      deleteContact(id);
+      deleteContact(_id);
       clearCurrent();
   }
   return (
